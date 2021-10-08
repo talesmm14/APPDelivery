bc9c2163202189b90caddca69656da4169936a4d
pat002900 <Aluno@Mac-mini-de-pat002900.local>
2021-10-07 21:37:04 -0300

    first commit

---
+++ APPDelivery/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  APPDelivery
+//
+//  Created by pat002900 on 07/10/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ APPDelivery/ProdutosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProdutosViewController.swift
+//  APPDelivery
+//
+//  Created by pat002900 on 07/10/21.
+//
+
+import UIKit
+
+class ProdutosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
